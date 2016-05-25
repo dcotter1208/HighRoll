418421bf08144f15b391fa724b02c4e18d748709
DetroitLabs <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-05-25 10:09:38 -0400

    first commit

---
+++ HighRoll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HighRoll
+//
+//  Created by DetroitLabs on 5/25/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HighRoll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HighRoll
+//
+//  Created by DetroitLabs on 5/25/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
